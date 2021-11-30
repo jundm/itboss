b88b1fb91b46641c5fcb05a4fa708fd498b06d6f
Hyunsungjun <bnmva23@hanmail.net>
2021-11-30 19:39:26 +0900

    feat:craco,prettier,route,codeSplitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from '@/layouts/Main';
+import loadable from '@loadable/component';
+
+// 코드 스플리팅 (페이지 단위로 하는게 좋다)
+const LogIn = loadable(() => import('@/pages/LogIn'));
+const SignUp = loadable(() => import('@/pages/SignUp'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/layouts/Main/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Main() {
+  return <div>메인이에용</div>;
+}
+
+export default Main;
