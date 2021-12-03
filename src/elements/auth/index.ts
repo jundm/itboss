ab5447c64d3dc7f98a8b0e057b21c196e6a747e5
Hyunsungjun <bnmva23@hanmail.net>
2021-12-03 20:24:54 +0900

    feat: 로그아웃 메세지 추가

--- src/elements/auth/index.ts
+++ src/elements/auth/index.ts
@@ -1,12 +1,18 @@
 import { auth } from "@/utils/Firebase/firebaseConfig";
 import { signInWithEmailAndPassword, signOut } from "@firebase/auth";
+import Swal from "sweetalert2";
 
 export const signOutButton = () => {
   signOut(auth)
     .then(() => {
-      console.log("로그아웃 되었습니다");
+      Swal.fire({
+        icon: "success",
+        title: "로그아웃 완료😎",
+        showConfirmButton: false,
+        timer: 1500,
+      });
     })
     .catch((error) => {
-      console.log("error", error);
+      // console.log("error", error);
     });
 };
